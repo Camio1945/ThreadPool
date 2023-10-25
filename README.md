a9f70dd02cc4b7be343d02b70ea7293ad42fba6b
HuKaiXuan <hukaixuan@up72.com>
2023-10-25 10:57:07 +0800

    增加中文注释

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
+### 改动说明
+
+1. 在 ThreadPool.h 和 example.cpp 中增加了中文注释。
+2. 把少数一些嵌套的变量提取出来了，方便调试。如：
+
+把
+```c++
+auto task = std::make_shared< std::packaged_task<return_type()> >(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+```
+改成了
+```c++
+auto callableBind = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+auto task = std::make_shared< std::packaged_task<return_type()> >(callableBind);
+```
+3. 通过clang-format格式化了代码。
+4. 给条件判断后的单行语句增加了括号。如：
+
+把
+```c++
+for (auto &&result: results)
+  std::cout << result.get() << ' ';
+```
+改成了
+```c++
+for (auto &&result: results) {
+  std::cout << result.get() << ' ';
+}
+```
+5. example.cpp 中增加耗时统计。
+
+
+---
+
 ThreadPool
 ==========
 
--- example.cpp
+++ example.cpp
@@ -1,29 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
+#include <chrono>  // 用于时间相关操作，比如 std::chrono::seconds
+#include <iostream>// 用于输入输出，比如 std::cout，间接引用了 time.h ，因此可以获取当前时间
+#include <vector>  // 元素容器，可理解为一个变长的数组，比如 std::vector
 
-#include "ThreadPool.h"
+#include "ThreadPool.h"// 引入头文件
 
-int main()
-{
-    
-    ThreadPool pool(4);
-    std::vector< std::future<int> > results;
+int main() {
+  clock_t start = clock();// 记录开始时间
 
-    for(int i = 0; i < 8; ++i) {
-        results.emplace_back(
-            pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
-                return i*i;
-            })
-        );
-    }
+  int thread_number = 3;                                   // 线程池中线程的数量
+  int task_number = 12;                                    // 任务数量
+  ThreadPool pool(thread_number);                          // 创建一个线程池，参数是线程池中线程的数量【类】
+  std::vector<std::future<int>> results;                   // 用于保存线程执行结果的容器【::命名空间】【泛型】
+  results.reserve(task_number);                            // 预留空间，避免内存重新分配
+  for (int i = 0; i < task_number; ++i) {                  // 循环创建若干个任务
+    // lambda 表达式，代表一个要执行的任务，参数和返回值都是int类型【lambda】【auto】
+    auto taskLambda = [i] {
+      std::cout << "hello " << i << std::endl;             // 打印 hello【打印】
+      std::this_thread::sleep_for(std::chrono::seconds(1));// 睡眠1秒
+      std::cout << "world " << i << std::endl;             // 打印 world
+      return i * i;                                        // 返回 i 的平方
+    };
+    auto future = pool.enqueue(taskLambda); // 将任务放入线程池，返回一个 std::future<int> 对象
+    // 将 std::future<int> 对象放入容器，这里使用了移动语义，表示对 future 进行引用，而不是复制【move】
+    results.emplace_back(std::move(future));
+  }
+  for (auto &&result: results) {// 循环打印线程执行结果，&& 表示对 result 进行引用，而不是复制【&&】
+    std::cout << result.get() << ' ';
+  }
 
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
-    
-    return 0;
+  // 打印总耗时（秒数）
+  std::cout << "\ntime: " << (double) (clock() - start) / CLOCKS_PER_SEC << "s" << std::endl;
+  return 0;
 }
